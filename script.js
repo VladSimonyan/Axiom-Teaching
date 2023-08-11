fea465c51756f567d7344c62e2d96002888b8f0c
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-08-10 20:16:23 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSelectedSubject() {
+  const dropdown = document.getElementById('subjectDropdown');
+  const selectedSubject = dropdown.value;
+  const contentDiv = document.getElementById('content');
+  
+  // ... rest of the JavaScript code ...
+}
